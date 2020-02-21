f277b304191a06783c49f44731feba44f19e7996
Tiago M. Silva <tmsilva11@gmail.com>
2020-02-21 17:01:03 -0300

    Invisible but functional menu
    Added multiplayer support for healthUI, and list of players in gameManager.

---
+++ Assets/DeviceManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DeviceManager : MonoBehaviour
+{
+    [SerializeField]
+    private static int PlayerCount = 0;
+
+    [SerializeField]
+    public static InputDevice[] PlayerDevices { get; } = new InputDevice[4];
+
+    [SerializeField]
+    private GameObject playerPrefab;
+
+    public void InstantiatePlayers()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            if(PlayerDevices[i] != null)
+            {
+                PlayerInputManager pm = GetComponent<PlayerInputManager>();
+                pm.playerPrefab = playerPrefab;
+                pm.JoinPlayer(pairWithDevice: PlayerDevices[i]);
+                Debug.Log("Joining player");
+            }
+        }
+    }
+
+    void OnPlayerJoined(PlayerInput playerInput)
+    {
+        Debug.Log("Joined" + playerInput.playerIndex);
+        PlayerDevices[playerInput.playerIndex] = playerInput.devices[0];
+        PlayerCount++;
+
+        playerInput.gameObject.transform.Translate(Vector3.up * 10);
+    }
+
+    /*void OnPlayerLeft(PlayerInput playerInput)
+    {
+        Debug.Log("Left" + playerInput.playerIndex);
+        PlayerDevices[playerInput.playerIndex] = null;
+        PlayerCount--;
+    }*/
+}
---
+++ Assets/PlayerUIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerUIController : MonoBehaviour
+{
+    public event Action playerJoined;
+    public event Action playerCanceled;
+
+    void OnCancel()
+    {
+        Debug.Log("Cancel event");
+        playerCanceled?.Invoke();
+        SceneManager.LoadScene("PlayerScene");
+    }
+
+    void OnJoin()
+    {
+        Debug.Log("OnJoin event");
+        playerJoined?.Invoke();
+    }
+}
